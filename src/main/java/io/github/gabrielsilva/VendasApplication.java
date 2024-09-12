a17bff018c1d943249cd86d95056289a28e55c87
Gabriel Silva <gmsilva08@gmail.com>
2024-09-12 11:44:28 -0300

    Acesso a Dados utilizando JDBC Template

--- src/main/java/io/github/gabrielsilva/VendasApplication.java
+++ src/main/java/io/github/gabrielsilva/VendasApplication.java
@@ -1,38 +1,53 @@
 package io.github.gabrielsilva;
 
+import io.github.gabrielsilva.domain.entity.Cliente;
+import io.github.gabrielsilva.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
-    @Value("${application.name}")
-    private String applicationName;
-
-    @Gato
-    private Animal animal;
-
     @Bean
-    public CommandLineRunner executar(){
+    public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
-            this.animal.fazerBarulho();
+            System.out.println("Salvando Clientes");
+            clientes.salvar(new Cliente("Gabriel"));
+            clientes.salvar(new Cliente("Poliana"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando Clientes");
+            todosClientes.forEach(c -> {c.setNome(c.getNome() + " atualizado");
+            clientes.atualizar(c);
+        });
+            todosClientes.forEach(System.out::println);
+            System.out.println("Buscando Clientes");
+            clientes.buscarPorNome("Ga").forEach(System.out::println);
+
+            System.out.println("Deletando Clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            }else{
+                todosClientes.forEach(System.out::println);
+            }
+
         };
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return applicationName;
-    }
-
     public static void main(String[] args) {
+
         SpringApplication.run(VendasApplication.class, args);
     }
 }
