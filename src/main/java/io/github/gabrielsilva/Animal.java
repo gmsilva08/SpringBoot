c1211ceb046a455f7bb5525bdb265c9c89e9b754
Gabriel Silva <gmsilva08@gmail.com>
2024-09-12 11:31:43 -0300

    Spring Boot Core e Spring Framework

---
+++ src/main/java/io/github/gabrielsilva/Animal.java
@@ -0,0 +1,5 @@
+package io.github.gabrielsilva;
+
+public interface Animal {
+    void fazerBarulho();
+}
---
+++ src/main/java/io/github/gabrielsilva/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.gabrielsilva.repository;
+
+import io.github.gabrielsilva.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //Acessa a base e salva o cliente
+    }
+}
